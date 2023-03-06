a20ad4e592acb4bcc5860bc6ba2ccc2281e3b2bd
sji <sji@sj-i.dev>
2023-03-06 15:08:53 +0900

    index の追加 61256
    https://github.com/narusejun/isucon12-final/commit/e260a1fb34af2e1728c25b5e0a8cd0e66bd7d133

--- app/webapp/sql/setup/1_schema.sql
+++ app/webapp/sql/setup/1_schema.sql
@@ -146,7 +146,8 @@ CREATE TABLE `user_presents` (
   `updated_at`bigint NOT NULL,
   `deleted_at` bigint default NULL,
   PRIMARY KEY (`id`),
-  INDEX userid_idx (`user_id`)
+  KEY (`id`, `deleted_at`),
+  KEY (`user_id`, `deleted_at`, `created_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 /* ガチャマスタ */
