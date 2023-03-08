8be3c7430aa547f55c43ad28585cfbde98fe5fda
sji <sji@sj-i.dev>
2023-03-09 01:16:20 +0900

    というかキャッシュ更新の対応がおかしかったので修正

--- app/webapp/php/src/IsuConquest/MasterCache.php
+++ app/webapp/php/src/IsuConquest/MasterCache.php
@@ -44,49 +44,46 @@ class MasterCache
 
     public function recache(PDO $db, string $master_version)
     {
-        $this->cache->saveDeferred($this->cache->getItem('master_version')->set($master_version));
+        $master_version_cache = $this->cache->getItem('master_version');
+        $login_bonus_masters_cache = $this->cache->getItem('login_bonus_masters');
+        $item_masters_cache = $this->cache->getItem('item_masters');
+        $gacha_masters_cache = $this->cache->getItem('gacha_masters');
+        $gacha_item_masters_cache = $this->cache->getItem('gacha_item_masters');
+        $present_all_masters_cache = $this->cache->getItem('present_all_masters');
+        $login_bonus_reward_masters_cache = $this->cache->getItem('login_bonus_reward_masters');
+
+        $this->cache->saveDeferred($master_version_cache->set($master_version));
         $this->cache->saveDeferred(
-            $this->cache->getItem('login_bonus_masters')
-                ->set(
-                    $db->query('SELECT * FROM login_bonus_masters')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $login_bonus_masters_cache->set(
+                $db->query('SELECT * FROM login_bonus_masters')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
         $this->cache->saveDeferred(
-            $this->cache->getItem('item_masters')
-                ->set(
-                    $db->query('SELECT * FROM item_masters')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $item_masters_cache->set(
+                $db->query('SELECT * FROM item_masters')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
         $this->cache->saveDeferred(
-            $this->cache->getItem('gacha_masters')
-                ->set(
-                    $db->query('SELECT * FROM gacha_masters ORDER BY display_order')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $gacha_masters_cache->set(
+                $db->query('SELECT * FROM gacha_masters ORDER BY display_order')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
         $this->cache->saveDeferred(
-            $this->cache->getItem('gacha_masters')
-                ->set(
-                    $db->query('SELECT * FROM gacha_masters ORDER BY display_order')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $gacha_item_masters_cache->set(
+                $db->query('SELECT * FROM gacha_item_masters ORDER BY id ASC')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
         $this->cache->saveDeferred(
-            $this->cache->getItem('gacha_item_masters')
-                ->set(
-                    $db->query('SELECT * FROM gacha_item_masters ORDER BY id ASC')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $present_all_masters_cache->set(
+                $db->query('SELECT * FROM present_all_masters')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
         $this->cache->saveDeferred(
-            $this->cache->getItem('present_all_masters')
-                ->set(
-                    $db->query('SELECT * FROM present_all_masters')->fetchAll(PDO::FETCH_ASSOC)
-                )
+            $login_bonus_reward_masters_cache->set(
+                $db->query('SELECT * FROM login_bonus_reward_masters')->fetchAll(PDO::FETCH_ASSOC)
+            )
         );
-        $this->cache->saveDeferred(
-            $this->cache->getItem('login_bonus_reward_masters')
-                ->set(
-                    $db->query('SELECT * FROM login_bonus_reward_masters')->fetchAll(PDO::FETCH_ASSOC)
-                )
-        );
+        $this->cache->commit();
     }
 
     public function getLoginBonusMaster(int $requestAt): array
